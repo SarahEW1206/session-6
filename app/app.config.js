2de8f1c7916660762a1069db78fdf611d24e3ef0
Sarah Waldie <sareliz126@gmail.com>
2016-10-27 15:12:08 -0400

    Added ingredients and directions

--- app/app.config.js
+++ app/app.config.js
@@ -1,12 +1,14 @@
-angular.module('recipeApp').config(
-    function config($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
-        $routeProvider.
-            when('/', {
-                template: '<recipe-list></recipe-list>'
-            }).
-            when('/recipes/:recipeId', {
-                template: '<recipe-detail></recipe-detail>'
-            }).
-            otherwise('/recipes');
-    });
+angular.module('recipeApp').
+    config(['$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
+            $locationProvider.hashPrefix('!');
+            $routeProvider.
+                when('/', {
+                    template: '<recipe-list></recipe-list>'
+                }).
+                when('/recipes/:recipeId', {
+                    template: '<recipe-detail></recipe-detail>'
+                }).
+                otherwise('/recipes');
+        }
+    ]);
